db37ef6079687351a771a532cfb26880168fc201
McCoy <8932344+mjamison3@users.noreply.github.com>
2019-09-28 15:41:02 -0400

    added a filter to retrieve items with discount

--- checkout.js
+++ checkout.js
@@ -1,5 +1,9 @@
-function selectSaleItems(items) {
-  return items
+/*this function filters through the selectSalesItems function to retrieve the items with discounts*/
+function selectSaleItems(items) { 
+  return items.filter((discountYes) => {
+    return discountYes.discount > 0;
+  }) 
 }
+
 
 module.exports = selectSaleItems
